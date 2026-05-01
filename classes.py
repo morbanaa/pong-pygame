025467948abb8235898972bef3559f38d11e4b94
Morbanaa <theodorerodd@gmail.com>
2026-05-01 18:35:00 -0500

    Created parent object class with contructor and created three sub classes of the parent

--- classes.py
+++ classes.py
@@ -1,1 +1,14 @@
-ECHO is on.
+class GameObjects():
+    def __init__(self,xpos,ypos,speed):
+        self.xpos = xpos
+        self.ypos = ypos
+        self.speed = speed
+
+class PlayerOne(GameObjects):
+    pass
+
+class PlayerTwo(GameObjects):
+    pass
+
+class Ball(GameObjects):
+    pass
