ba65e69965a4eea6fb86cc1171f496d925a104d2
Morbanaa <theodorerodd@gmail.com>
2026-05-01 18:40:25 -0500

    Set up main function and created the window using CONST vars

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-ECHO is on.
+import pygame
+
+GAME_WIDTH = 1280
+GAME_HEIGHT = 720
+
+def main():
+    # set up the window
+    pygame.init()
+    screen = pygame.display.set_mode((GAME_HEIGHT,GAME_HEIGHT))
+    pygame.display.set_caption("Pong!")
+
+
+# Program Entry Point
+if __name__ == "__main__":
+    main()
