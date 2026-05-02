a51fb6d7b2f308e9342af40c948d133f12904ee2
Morbanaa <theodorerodd@gmail.com>
2026-05-01 19:05:33 -0500

    Created game objects p1 p2 and ball,now draws the game objects each frame at the correct starting point

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from classes import PlayerOne,PlayerTwo,Ball
 
 GAME_WIDTH = 1280
 GAME_HEIGHT = 720
@@ -6,26 +7,43 @@ GAME_HEIGHT = 720
 def main():
     # set up the window
     pygame.init()
-    screen = pygame.display.set_mode((GAME_HEIGHT,GAME_HEIGHT))
+    screen = pygame.display.set_mode((GAME_WIDTH,GAME_HEIGHT))
     pygame.display.set_caption("Pong!")
 
     # Create game objects
     clock = pygame.time.Clock()
+    player_one = PlayerOne(20,GAME_HEIGHT//2,5)
+    player_two = PlayerTwo(GAME_WIDTH - 40,GAME_HEIGHT//2,5)
+    ball = Ball(GAME_WIDTH//2,GAME_HEIGHT//2,3)
 
     running = True
     while running:
         clock.tick(60) # Game Runs at 60 FPS
 
+        ##############################################
         # Input
+        ##############################################
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-
+        
+        ##############################################
         # Update
+        ##############################################
 
+        ##############################################
         # Draw
+        ##############################################
+        #Draw Background
         screen.fill((0,200,0))
-        pygame.display.update()
+        #Draw PlayerOne
+        pygame.draw.rect(screen,(0,0,0),(player_one.xpos,player_one.ypos,20,100))
+        #Draw PlayerTwo
+        pygame.draw.rect(screen,(0,0,0),(player_two.xpos,player_two.ypos,20,100))
+        #Draw Ball
+        pygame.draw.circle(screen,(255,0,0),(GAME_WIDTH//2,GAME_HEIGHT//2),10)
+
+        pygame.display.update() # Apply Changes
     
     # Closes out of window
     pygame.quit()
