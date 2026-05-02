4bfcf45d67fed3b0ad9cf8a0482be0fc8efd070f
Morbanaa <theodorerodd@gmail.com>
2026-05-02 03:36:50 -0500

    Created update method which returns the balls change in x and y

--- classes.py
+++ classes.py
@@ -1,3 +1,5 @@
+import random
+
 class GameObjects():
     def __init__(self,xpos,ypos,speed):
         self.xpos = xpos
@@ -11,4 +13,21 @@ class PlayerTwo(GameObjects):
     pass
 
 class Ball(GameObjects):
-    pass
+    def __init__(self,xpos,ypos,speed,direction,angle):
+        super().__init__(xpos,ypos,speed)
+        self.direction = direction
+        self.angle = angle
+
+    def update(self):
+        if self.direction == 1:
+            self.xpos -= self.speed
+        elif self.direction == 2:
+            self.xpos += self.speed
+
+        if self.direction == 1:
+            self.ypos -= self.angle
+        elif self.direction == 2:
+            self.ypos += self.angle
+        
+
+        return self.xpos,self.ypos
